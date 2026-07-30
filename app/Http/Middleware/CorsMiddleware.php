40a82ebb6518cd47c21fd8404f8766e6f442a6bb
Surya <157574400+0710surya@users.noreply.github.com>
2026-07-30 11:58:22 +0530

    Slider change + meta title added

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -26,9 +26,10 @@ class CorsMiddleware
 
         $response = $next($request);
 
-        return $response
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Headers', '*');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', '*');
+        $response->headers->set('Access-Control-Allow-Headers', '*');
+
+        return $response;
     }
 }
